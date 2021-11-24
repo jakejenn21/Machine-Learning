2c651faa8c5bc8a5ec2030333d89aacf080c3880
Jacob Jenn <jakejenn21@gmail.com>
2021-11-24 15:19:48 -0700

    Added code structure for homework 5

--- run.sh
+++ run.sh
@@ -19,6 +19,10 @@
 
 #python3 Perceptron/PerceptronTest.py
 
-python3 SVM/SVMTests.py
+#python3 SVM/SVMTests.py
+
+python3 "Neural Networks/NeuralNetTests.py"
+
+python3 "Logistic Regression/LogisticRegressionTests.py"
 
 
